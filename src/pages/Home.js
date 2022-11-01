a543b05bde0b44497903f3e5ec231f7168079190
Takumi <t-nakayama-1w1@eagle.sophia.ac.jp>
2022-11-01 12:52:04 +0900

    add example using rakuten api

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,13 +1,41 @@
-import React, { useEffect } from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 const Home = () => {
 
-    //useEffect()
+    const [msg, setMsg] = useState("");
+    const apiUrl = "https://app.rakuten.co.jp/services/api/BooksTotal/Search/20170404";
+
+    const a = (m) => {
+        const p = {
+            format: "json",
+            applicationId: process.env.REACT_APP_APPLICATION_ID,
+            isbnjan: "9784908686153",
+        }
+        if (m !== "") {
+            axios.get(apiUrl, {
+                params: p
+            })
+                .then(function (response) {
+                    console.log(response);
+                })
+                .catch(function (error) {
+                    console.log(error);
+                });
+        }
+        setMsg("");
+    }
+
+
     return (
         <div>
-            This is Home Page
+            <p>home page.</p>
+            <input type="text" value={msg} placeholder="入力してください" onChange={(e) => setMsg(e.target.value)}></input>
+            <button onClick={() => a(msg)}>
+                Click me
+            </button>
         </div>
-    )
+    );
 }
 
 export default Home;
