3f6eabc7fae2d2fc468e97ec3058a30069722e72
detteiu55 <detteiu5522@gmail.com>
2022-11-07 16:05:25 +0900

    テキストボックスがふえました

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,20 +6,28 @@ const Home = () => {
     const [msg, setMsg] = useState("");
     const [result, setResult] = useState({
         title: "",
-        author: ""
+        author: "",
+        publisher: "",
+        jan: "",
+        salesDate: ""
     });
 
     const handleChange = (input) => e => {
         setResult({...result, [input] : e.target.value});
     };
 
+
+    const handleSubmit = (e) => {
+        console.log(result);
+    }
+
     const apiUrl = "https://app.rakuten.co.jp/services/api/BooksTotal/Search/20170404";
 
-    const a = (m) => {
+    const getInfo = (m) => {
         const p = {
             format: "json",
             applicationId: process.env.REACT_APP_APPLICATION_ID,
-            isbnjan: "9784908686153",
+            isbnjan: m,
         }
         if (m !== "") {
             axios.get(apiUrl, {
@@ -30,7 +38,10 @@ const Home = () => {
                     setResult(
                         {
                             title: response.data.Items[0].Item.title,
-                            author: response.data.Items[0].Item.author
+                            author: response.data.Items[0].Item.author,
+                            publisher: response.data.Items[0].Item.publisherName,
+                            jan: m,
+                            salesDate: response.data.Items[0].Item.salesDate
                         });
                 })
                 .catch(function (error) {
@@ -44,13 +55,18 @@ const Home = () => {
     return (
         <div>
             <p>home page.</p>
-            <input type="text" value={msg} placeholder="入力してください" onChange={(e) => setMsg(e.target.value)}></input>
-            <button onClick={() => a(msg)}>
-                Click me
+            <input type="text" value={msg} placeholder="JANコードを入力してください" onChange={(e) => setMsg(e.target.value)}></input>
+            <button onClick={() => getInfo(msg)}>
+                Search
             </button>
-            <form>
-                <input type="text" name="title" value={result.title} onChange={handleChange("title")} />
-                <input type="text" name="author" value={result.author} onChange={handleChange("author")} />
+            <br></br>
+            <br></br>
+            <form onSubmit={handleSubmit}>
+                Title: <input type="text" id="titleBox" value={result.title} onChange={handleChange("title")} /><br></br>
+                Author: <input type="text" id="authorBox" value={result.author} onChange={handleChange("author")} /><br></br>
+                Publisher: <input type="text" id="publisherBox" value = {result.publisher} onChange={handleChange("publisher")} /><br></br>
+                JAN-code: <input type="text" id="janBox" value={result.jan} onChange={handleChange("jan")} /><br></br>
+                salesDate: <input type="text" id="salesDateBox" value={result.salesDate} onChange={handleChange("salesDate")} /><br></br>
                 <input type="submit" value="Submit" />
             </form>
         </div>
