9be95b9aacae092a1e49578984abc3efc94f8ed9
bata-yas <85926985+bata-yas@users.noreply.github.com>
2022-11-08 15:32:29 +0900

    [bugfix] delete Sample

--- src/index.js
+++ src/index.js
@@ -5,12 +5,10 @@ import App from './App';
 import { Container, Divider, Header, Segment, Table, Button } from 'semantic-ui-react'
 import 'semantic-ui-css/semantic.min.css'
 
-const Sample = () => {
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+        <React.StrictMode>
+                <App />
+        </React.StrictMode>
+);
 
-        const root = ReactDOM.createRoot(document.getElementById('root'));
-        root.render(
-                <React.StrictMode>
-                        <App />
-                </React.StrictMode>
-        );
-}
